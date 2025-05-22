50a0e0026659b5d7c7335efacdea7414f0aa3b80
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-05-23 01:36:39 +0700

    Menambahkan setup lokasi awal dan mendapatkan alamat saat aplikasi dibuka

---
+++ lib/screen/getlocation/mappage.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _ctrl = Completer();
+  Marker? _pickedMarker;
+  String? _currentAddress;
+  String? _pickedAddress;
+  CameraPosition? _initialCamera;
+  Position? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupLocation();
+  }
+
+  Future<void> _setupLocation() async {
+    try {
+      final pos = await getPermissions();
+      _currentPosition = pos;
+      _initialCamera = CameraPosition(
+        target: LatLng(pos.latitude, pos.longitude),
+        zoom: 16,
+      );
+      final placemarks = await placemarkFromCoordinates(
+        _currentPosition!.longitude,
+        _currentPosition!.latitude,
+      );
+
+      final p = placemarks.first;
+      _currentAddress = '${p.name}, ${p.locality}, ${p.country}';
+
+      setState(() {});
+    } catch (e) {
+      //jika gagal(denied, service off), fallback ke view global
+      _initialCamera = const CameraPosition(target: LatLng(0, 0), zoom: 2);
+      setState(() {});
+      print(e);
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+  }
+}
