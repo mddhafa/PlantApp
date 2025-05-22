bfc647e5a5da39589aef979730cb2780bd23adfa
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-05-22 23:47:52 +0700

    membuat card untuk recomendasi produk

---
+++ lib/screen/components/recomend_plants.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+const double kDefaultPadding = 20.0;
+const Color kPrimaryColor = Color(0xFF00DDFF);
+
+class RecomendPlantCard extends StatelessWidget {
+  const RecomendPlantCard({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.country,
+    required this.price,
+  });
+
+  final String image, title, country;
+  final int price;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.only(
+        left: kDefaultPadding,
+        top: kDefaultPadding / 2,
+        bottom: kDefaultPadding * 2.5,
+      ),
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          Image.asset(image),
+          Container(
+            padding: const EdgeInsets.all(kDefaultPadding / 2),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  offset: const Offset(0, 10),
+                  blurRadius: 50,
+                  color: kPrimaryColor.withOpacity(0.23),
+                ),
+              ],
+            ),
+            child: Row(
+              children: <Widget>[
+                RichText(
+                  text: TextSpan(
+                    style: DefaultTextStyle.of(context).style,
+                    children: [
+                      TextSpan(
+                        text: "$title\n".toUpperCase(),
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: country.toUpperCase(),
+                        style: TextStyle(color: kPrimaryColor.withOpacity(0.5)),
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                Text(
+                  '\$$price',
+                  style: Theme.of(
+                    context,
+                  ).textTheme.labelLarge?.copyWith(color: kPrimaryColor),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
