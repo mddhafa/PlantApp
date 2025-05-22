85b71cba0a7a57a5bd98edb9ed5bb801c271d600
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-05-23 00:47:42 +0700

    Membuat featuterdplant yang berisi gambar produk

---
+++ lib/screen/components/featurred_plants.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+const double kDefaultPadding = 20.0;
+const Color kPrimaryColor = Color(0xFF00DDFF);
+
+class FeaturedPlants extends StatelessWidget {
+  const FeaturedPlants({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          FeaturePlantCard(image: "assets/images/produk1.jpg", press: () {}),
+          FeaturePlantCard(image: "assets/images/produk2.png", press: () {}),
+        ],
+      ),
+    );
+  }
+}
+
+class FeaturePlantCard extends StatelessWidget {
+  const FeaturePlantCard({Key? key, required this.image, required this.press})
+    : super(key: key);
+
+  final String image;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: const EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
+        ),
+      ),
+    );
+  }
+}
